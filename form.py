24fe53b743256f02fd6e5b03fcb29196b4605d81
Ini-Abasi Affiah <iniabasi.bernard@gmail.com>
2021-11-18 13:09:50 +0100

    align text box in form

--- form.py
+++ form.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired
 
 class SoilParameters(FlaskForm):
     '''Form to input the soil parameters'''
-    nitrogen = IntegerField('Nitrogen        ', validators=[DataRequired()])
-    phosphorous = IntegerField('Phosphorous     ', validators=[DataRequired()])
-    potassium = IntegerField('Potassium       ', validators=[DataRequired()])
-    temperature = FloatField('Temperature     ', validators=[DataRequired()])
-    humidity = FloatField('Humidity        ', validators=[DataRequired()])
-    pH = FloatField('pH              ', validators=[DataRequired()])
-    rainfall = FloatField('Rainfall        ', validators=[DataRequired()])
+    nitrogen = IntegerField('Nitrogen', validators=[DataRequired()])
+    phosphorous = IntegerField('Phosphorous', validators=[DataRequired()])
+    potassium = IntegerField('Potassium', validators=[DataRequired()])
+    temperature = FloatField('Temperature', validators=[DataRequired()])
+    humidity = FloatField('Humidity', validators=[DataRequired()])
+    pH = FloatField('pH', validators=[DataRequired()])
+    rainfall = FloatField('Rainfall', validators=[DataRequired()])
     send = SubmitField('Check')
